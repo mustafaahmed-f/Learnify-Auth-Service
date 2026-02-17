e326cc95781679f4e9a4c549e87d79849f2ef2a1
Mustafa Ahmed <mostafafikry97@gmail.com>
2026-02-17 19:07:31 +0200

    ✨ feat(prisma_and_structure): finished the main folders structure and installed prisma with mysql. finally added mysql and phpmyadmin images to the docker compose file

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ const app = express();
 app.use(express.json());
 app.get("/", (req, res) => res.send("Hello world !!"));
 
-app.listen(5000, () => console.log("Server is running on port 5000"));
+app.listen(process.env.PORT, () =>
+  console.log(`Server is running on port ${process.env.PORT}`),
+);
 
 console.log("Running ...... ");
